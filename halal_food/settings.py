b73e6407f8a14eedaddfc4b2777e0e9e406d7e6c
abrorbek_anvarbekov <1aabror@gmail.com>
2023-02-10 06:40:25 +0900

    *

--- halal_food/settings.py
+++ halal_food/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p_ilc)#8*w3zqz4#j$&8o=u34z6&mf)k505f6xrxs=678($07_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
